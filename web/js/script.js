fa2e48900442eec15f5011e5c48c1bf1e2ef8158
Raimundo Neto <raimundo.n.neto10@gmail.com>
2023-11-07 17:15:04 -0300

    teste para validação

--- web/js/script.js
+++ web/js/script.js
@@ -4,3 +4,16 @@ const emailInput = document.querySelector("#email")
 const passwordInput = document.querySelector("#password")
 const locationdSelect = document.querySelector("#location")
 const messageTextarea = document.querySelector("#message")
+
+/*form.addEventListener("submit").Event(
+    Event.preventDefault()
+);*/
+
+
+form.addEventListener("submit", (Event) => {
+    Event.preventDefault();
+    if(nameinput.value === null ){
+        alert("Campo 'Nome' não pode ser vazio!");
+        return;
+    }
+});
